a88fcc3786dd0cda270f826f70abea2c8409dad6
Geoffrey Beausire <g.beausire@criteo.com>
2017-11-27 14:39:41 +0100

    Changed toolbar live-view position

--- tailon/assets/gen/Main.js
+++ tailon/assets/gen/Main.js
@@ -1,105 +1,3 @@
-var Utils;
-(function (Utils) {
-    function formatBytes(size) {
-        var units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-        var i = 0;
-        while (size >= 1024) {
-            size /= 1024;
-            ++i;
-        }
-        return size.toFixed(1) + ' ' + units[i];
-    }
-    Utils.formatBytes = formatBytes;
-    function formatFilename(state) {
-        if (!state.id)
-            return state.text;
-        var size = formatBytes($(state.element).data('size'));
-        return '<span>' + state.text + '</span>' + '<span style="float:right;">' + size + '</span>';
-    }
-    Utils.formatFilename = formatFilename;
-    function endsWith(str, suffix) {
-        return str.indexOf(suffix, str.length - suffix.length) !== -1;
-    }
-    Utils.endsWith = endsWith;
-    function startsWith(str, prefix) {
-        return str.indexOf(prefix) === 0;
-    }
-    Utils.startsWith = startsWith;
-    var escape_entity_map = {
-        "&": "&amp;",
-        "<": "&lt;",
-        ">": "&gt;",
-        "/": '&#x2F;'
-    };
-    // This is the escapeHtml function from mustache.js.
-    function escapeHtml(str) {
-        return String(str).replace(/[&<>\/]/g, function (s) {
-            return escape_entity_map[s];
-        });
-    }
-    Utils.escapeHtml = escapeHtml;
-    function parseQueryString(str) {
-        var res = {};
-        str.substr(1).split('&').forEach(function (item) {
-            var el = item.split("=");
-            var key = el[0];
-            var value = el[1] && decodeURIComponent(el[1]);
-            if (key in res) {
-                res[key].push(value);
-            }
-            else {
-                res[key] = [value];
-            }
-        });
-        return res;
-    }
-    Utils.parseQueryString = parseQueryString;
-    var Signal = /** @class */ (function () {
-        function Signal() {
-            this.listeners = [];
-        }
-        Signal.prototype.addCallback = function (callback) {
-            this.listeners.push(callback);
-        };
-        Signal.prototype.removeObserver = function (observer) {
-            this.listeners.splice(this.listeners.indexOf(observer), 1);
-        };
-        Signal.prototype.trigger = function (data) {
-            this.listeners.forEach(function (callback) {
-                callback(data);
-            });
-        };
-        return Signal;
-    }());
-    Utils.Signal = Signal;
-})(Utils || (Utils = {}));
-/// <reference path="Utils.ts" />
-var Settings;
-(function (Settings_1) {
-    var Settings = /** @class */ (function () {
-        function Settings(settings) {
-            this.settings = settings;
-            this.signals = {};
-            var keys = Object.keys(this.settings);
-            for (var i = 0; i < keys.length; i++) {
-                this.signals[keys[i]] = new Utils.Signal();
-            }
-        }
-        Settings.prototype.onChange = function (name, callback) {
-            this.signals[name].addCallback(callback);
-        };
-        Settings.prototype.set = function (key, value) {
-            console.log('settings key "' + key + '" set to "' + value + '"');
-            this.settings[key] = value;
-            this.signals[key].trigger(value);
-        };
-        Settings.prototype.get = function (key) {
-            return this.settings[key];
-        };
-        return Settings;
-    }());
-    Settings_1.Settings = Settings;
-})(Settings || (Settings = {}));
 var TailonServer = /** @class */ (function () {
     function TailonServer(apiURL, connectionRetries) {
         var _this = this;
@@ -286,6 +184,108 @@ var LogView = /** @class */ (function () {
 //         $('#wrap_lines').prop('checked', this.model.get('wrap-lines'));
 //     },
 // });
+var Utils;
+(function (Utils) {
+    function formatBytes(size) {
+        var units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+        var i = 0;
+        while (size >= 1024) {
+            size /= 1024;
+            ++i;
+        }
+        return size.toFixed(1) + ' ' + units[i];
+    }
+    Utils.formatBytes = formatBytes;
+    function formatFilename(state) {
+        if (!state.id)
+            return state.text;
+        var size = formatBytes($(state.element).data('size'));
+        return '<span>' + state.text + '</span>' + '<span style="float:right;">' + size + '</span>';
+    }
+    Utils.formatFilename = formatFilename;
+    function endsWith(str, suffix) {
+        return str.indexOf(suffix, str.length - suffix.length) !== -1;
+    }
+    Utils.endsWith = endsWith;
+    function startsWith(str, prefix) {
+        return str.indexOf(prefix) === 0;
+    }
+    Utils.startsWith = startsWith;
+    var escape_entity_map = {
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        "/": '&#x2F;'
+    };
+    // This is the escapeHtml function from mustache.js.
+    function escapeHtml(str) {
+        return String(str).replace(/[&<>\/]/g, function (s) {
+            return escape_entity_map[s];
+        });
+    }
+    Utils.escapeHtml = escapeHtml;
+    function parseQueryString(str) {
+        var res = {};
+        str.substr(1).split('&').forEach(function (item) {
+            var el = item.split("=");
+            var key = el[0];
+            var value = el[1] && decodeURIComponent(el[1]);
+            if (key in res) {
+                res[key].push(value);
+            }
+            else {
+                res[key] = [value];
+            }
+        });
+        return res;
+    }
+    Utils.parseQueryString = parseQueryString;
+    var Signal = /** @class */ (function () {
+        function Signal() {
+            this.listeners = [];
+        }
+        Signal.prototype.addCallback = function (callback) {
+            this.listeners.push(callback);
+        };
+        Signal.prototype.removeObserver = function (observer) {
+            this.listeners.splice(this.listeners.indexOf(observer), 1);
+        };
+        Signal.prototype.trigger = function (data) {
+            this.listeners.forEach(function (callback) {
+                callback(data);
+            });
+        };
+        return Signal;
+    }());
+    Utils.Signal = Signal;
+})(Utils || (Utils = {}));
+/// <reference path="Utils.ts" />
+var Settings;
+(function (Settings_1) {
+    var Settings = /** @class */ (function () {
+        function Settings(settings) {
+            this.settings = settings;
+            this.signals = {};
+            var keys = Object.keys(this.settings);
+            for (var i = 0; i < keys.length; i++) {
+                this.signals[keys[i]] = new Utils.Signal();
+            }
+        }
+        Settings.prototype.onChange = function (name, callback) {
+            this.signals[name].addCallback(callback);
+        };
+        Settings.prototype.set = function (key, value) {
+            console.log('settings key "' + key + '" set to "' + value + '"');
+            this.settings[key] = value;
+            this.signals[key].trigger(value);
+        };
+        Settings.prototype.get = function (key) {
+            return this.settings[key];
+        };
+        return Settings;
+    }());
+    Settings_1.Settings = Settings;
+})(Settings || (Settings = {}));
 // global $:false, jQuery:false
 // jshint laxcomma: true, sub: true
 /// <reference path="../vendor/typings/jquery.d.ts" />
