5c4cfaad207577c9aa76d707f04db1ecb35ac14a
Ramez Hanna <rhanna@informatiq.org>
2017-11-27 13:36:08 +0100

    use sift if available instead of zcat|grep

--- tailon/commands.py
+++ tailon/commands.py
@@ -16,6 +16,7 @@ class ToolPaths:
 
     def __init__(self, overwrites=None):
         self.cmd_grep  = self.first_in_path('grep')
+        self.cmd_sift  = self.first_in_path('sift')
         self.cmd_awk   = self.first_in_path('gawk', 'awk')
         self.cmd_sed   = self.first_in_path('gsed', 'sed')
         self.cmd_tail  = self.first_in_path('gtail', 'tail')
@@ -53,6 +54,14 @@ class CommandControl:
             cmd.append(fn)
         proc = process.Subprocess(cmd, stdout=stdout, stderr=stderr, **kw)
         log.debug('running grep %s, pid: %s', cmd, proc.proc.pid)
+        return proc
+
+    def sift(self, regex, fn, stdout, stderr, **kw):
+        cmd = [self.toolpaths.cmd_sift, '--zip','--binary-skip', '--no-color','--recursive', '-e', regex]
+        if fn:
+            cmd.append(fn)
+        proc = process.Subprocess(cmd, stdout=stdout, stderr=stderr, **kw)
+        log.debug('running sift %s, pid: %s', cmd, proc.proc.pid)
         return proc
 
     def sed(self, script, fn, stdout, stderr, **kw):
@@ -98,9 +107,13 @@ class CommandControl:
         return zcat, awk
 
     def all_grep(self, fn, regex, stdout, stderr):
-        zcat = self.zcat(fn, stdout=subprocess.PIPE, stderr=STREAM)
-        grep = self.grep(regex, None, stdout=STREAM, stderr=STREAM, stdin=zcat.stdout)
-        return zcat, grep
+        if self.toolpaths.cmd_sift:
+            grep = self.sift(regex, fn, stdout=STREAM, stderr=STREAM)
+            return grep
+        else:
+            zcat = self.zcat(fn, stdout=subprocess.PIPE, stderr=STREAM)
+            grep = self.grep(regex, None, stdout=STREAM, stderr=STREAM, stdin=zcat.stdout)
+            return zcat, grep
 
     def all_sed(self, fn, script, stdout, stderr):
         zcat = self.zcat(fn, stdout=subprocess.PIPE, stderr=STREAM)
