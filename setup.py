10360f34d7d057424f75295b42eb935f50590fda
Geoffrey Beausire <g.beausire@criteo.com>
2018-02-28 15:39:25 +0100

    Bump version: 1.3.0-criteo.4 -> 1.3.0-criteo.5

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requirements = [
 
 kw = {
     'name':             'tailon',
-    'version':          '1.3.0-criteo.4',
+    'version':          '1.3.0-criteo.5',
     'description':      'Webapp for looking at and searching through log files',
     'long_description': open('README.rst').read(),
     'author':           'Georgi Valkov',
--- tailon/__init__.py
+++ tailon/__init__.py
@@ -1,1 +1,1 @@
-__version__ = '1.3.0-criteo.4'
+__version__ = '1.3.0-criteo.5'
